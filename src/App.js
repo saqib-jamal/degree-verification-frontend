46645630b996537d4933b0ad28bfd3603edfe102
Jamal <saqibjamal734@gmail.com>
2026-08-21 01:07:19 +0500

    validate app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import DegreeVerification from './pages/Home';
 import Owner from './pages/Owner';
 import University from './pages/University';
+import Home from './pages/Home';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<DegreeVerification />} />
+        <Route path="/" element={<Home />} />
         <Route path="/owner" element={<Owner />} />
         <Route path="/university" element={<University />} />
       </Routes>
